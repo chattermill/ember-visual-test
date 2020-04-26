745f8f20ec7fdaa8c0d6d4772e073aa22a0774f5
Artem Gurzhii <artem.gurzhii@gmail.com>
2020-04-26 23:18:00 +0300

    use github actions instead of travis ci

--- README.md
+++ README.md
@@ -25,3 +25,12 @@ License
 ------------------------------------------------------------------------------
 
 This project is licensed under the [MIT License](LICENSE.md).
+
+
+## Notes
+This is the fork, here is the [original implementation](https://github.com/Cropster/ember-visual-test).
+
+It adds multiple features and removes few:
+1. It allows you to configure screen size, just use `await capture(assert, 'some-page', { windowWidth: 1680, windowHeight: 1050 });` and it will create a screenshot for that screen size(output png file will have same size as all others)
+2. Instead of `simple-headless-chrome` it uses `puppeteer`.
+3. Here we have also removed imgur image upload, as we do not need it for our use case and we don't want to maintain not needed logic.
--- config/ember-try.js
+++ config/ember-try.js
@@ -3,72 +3,90 @@
 const getChannelURL = require('ember-source-channel-url');
 
 module.exports = async function() {
-  return {
-    useYarn: true,
-    scenarios: [
-      {
-        name: 'ember-lts-3.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.4.0'
-          }
-        }
-      },
-      {
-        name: 'ember-lts-3.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.8.0'
-          }
-        }
-      },
-      {
-        name: 'ember-release',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('release')
-          }
-        }
-      },
-      {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta')
-          }
-        }
-      },
-      {
-        name: 'ember-canary',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('canary')
-          }
-        }
-      },
-      // The default `.travis.yml` runs this scenario via `npm test`,
-      // not via `ember try`. It's still included here so that running
-      // `ember try:each` manually or from a customized CI config will run it
-      // along with all the other scenarios.
-      {
-        name: 'ember-default',
-        npm: {
-          devDependencies: {}
-        }
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true
-          })
+  return Promise.all([
+    getChannelURL('release'),
+    getChannelURL('beta'),
+    getChannelURL('canary')
+  ]).then(urls => {
+    const releaseUrl = urls[0];
+    const betaUrl = urls[1];
+    const canaryUrl = urls[2];
+
+    return {
+      useYarn: true,
+      scenarios: [
+        {
+          name: 'ember-lts-3.4',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.4.0',
+            },
+          },
         },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1'
-          }
-        }
-      }
-    ]
-  };
+        {
+          name: 'ember-lts-3.8',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.8.0',
+            },
+          },
+        },
+        {
+          name: 'ember-lts-3.12',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.12.0',
+            },
+          },
+        },
+        {
+          name: 'ember-release',
+          npm: {
+            devDependencies: {
+              'ember-source': releaseUrl,
+            },
+          },
+        },
+        {
+          name: 'ember-beta',
+          npm: {
+            devDependencies: {
+              'ember-source': betaUrl,
+            },
+          },
+        },
+        {
+          name: 'ember-canary',
+          npm: {
+            devDependencies: {
+              'ember-source': canaryUrl,
+            },
+          },
+        },
+        // The default `.travis.yml` runs this scenario via `npm test`,
+        // not via `ember try`. It's still included here so that running
+        // `ember try:each` manually or from a customized CI config will run it
+        // along with all the other scenarios.
+        {
+          name: 'ember-default',
+          npm: {
+            devDependencies: {},
+          },
+        },
+        {
+          name: 'ember-default-with-jquery',
+          env: {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({
+              'jquery-integration': true,
+            }),
+          },
+          npm: {
+            devDependencies: {
+              '@ember/jquery': '^0.5.1',
+            },
+          },
+        },
+      ],
+    };
+  });
 };
